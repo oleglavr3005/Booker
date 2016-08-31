c83d46d88cb4f7218021d5c45cef54fed78546f9
Tanya_Tsymbaliak <tanya.tsymbaliak@gmail.com>
2016-08-31 18:34:10 +0300

    fixed hotel service

---
+++ src/main/java/com/epam/task/database/service/HotelService.java
@@ -0,0 +1,31 @@
+package com.epam.task.database.service;
+
+import java.util.List;
+
+import com.epam.task.database.dao.manager.DaoManager;
+import com.epam.task.database.model.Hotel;
+
+public class HotelService {
+private DaoManager daoManager;
+	
+	public HotelService(){
+		super();
+		daoManager = new DaoManager();
+	}
+	
+	public List<Hotel> getAllHotels() {
+		return daoManager.executeAndClose(() -> daoManager.getHotelDao().getAllHotels());
+	}
+	
+	public Hotel getHotelById(int id) {
+		return daoManager.executeAndClose(() -> daoManager.getHotelDao().getHotelById(id));
+	}
+	
+	public int insertHotel(Hotel hotel) {
+		return daoManager.executeAndClose(() -> daoManager.getHotelDao().insertHotel(hotel));
+	}
+	
+	public int deleteHotel(Hotel hotel){
+		return daoManager.executeAndClose(() -> daoManager.getHotelDao().deleteHotel(hotel));
+	}
+}
