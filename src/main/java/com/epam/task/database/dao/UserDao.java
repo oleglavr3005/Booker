04808aa7088d89b3fa4f3f0c4a7d3442e76895ec
NymphadoraTonks <lelich.katerina@mail.ru>
2016-08-31 15:14:02 +0300

    Added model, enums, dao for users

--- src/main/java/com/epam/task/database/dao/UserDao.java
+++ src/main/java/com/epam/task/database/dao/UserDao.java
@@ -3,13 +3,22 @@ package com.epam.task.database.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+
+import com.epam.task.database.model.User;
+import com.epam.task.database.transformers.UniversalTransformer;
 
 public class UserDao {
 
 	private Connection connection;
 	
-	private final String SQL_DELETE_USER = "DELETE FROM user " + "WHERE id = ?;";
+	private final String SELECT_ALL = "SELECT * FROM `user`";
+	private final String SELECT_BY_ID = SELECT_ALL + " WHERE user_id = ?";
+	//private final String SQL_DELETE_USER = "DELETE FROM user " + "WHERE id = ?;";
+	
+	private final String INSERT = "INSERT INTO `user` (first_name, last_name, e_mail, password, type, is_banned, confirm_code, status, phone_number, img, social_network, social_network_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	private final String UPDATE = "UPDATE `user` SET first_name = ?, last_name = ?, e_mail = ?, password = ?, type = ?, is_banned = ?, confirm_code = ?, status = ?, phone_number = ?, img = ?, social_network = ?, social_network_id = ? WHERE user_id = ?";
 
 	public UserDao(Connection connection) {
 		super();
@@ -20,5 +29,55 @@ public class UserDao {
 		this.connection = connection;
 	}
 
+	public List<User> getAllUsers() throws SQLException{
+		try (PreparedStatement statement = connection.prepareStatement(SELECT_ALL);
+					ResultSet result = statement.executeQuery()) {
+			return UniversalTransformer.getCollectionFromRS(result, User.class);
+		}
+	}
 	
+	public User getUserById() throws SQLException {
+		try (PreparedStatement statement = connection.prepareStatement(SELECT_BY_ID);
+				ResultSet result = statement.executeQuery()) {
+			return UniversalTransformer.getObjectFromRS(result, User.class);
+		}
+	}
+	
+	public int insertUser(User user) throws SQLException {
+		try (PreparedStatement statement = connection.prepareStatement(INSERT)) {
+			statement.setString(1, user.getFirstName());
+			statement.setString(2, user.getLastName());
+			statement.setString(3, user.getEmail());
+			statement.setString(4, user.getPassword());
+			statement.setString(5, user.getType().toString());
+			statement.setBoolean(6, user.isBanned());
+			statement.setString(7, user.getConfirmCode());
+			statement.setString(8, user.getStatus().toString());
+			statement.setString(9, user.getPhoneNumber());
+			statement.setString(10, user.getImage());
+			statement.setString(11, user.getSocialNetwork().toString());
+			statement.setString(12, user.getSocialNetworkId());
+			return statement.executeUpdate();
+		}
+	}
+	
+	public int updateUser(User user) throws SQLException {
+		try (PreparedStatement statement = connection.prepareStatement(UPDATE)) {
+			statement.setString(1, user.getFirstName());
+			statement.setString(2, user.getLastName());
+			statement.setString(3, user.getEmail());
+			statement.setString(4, user.getPassword());
+			statement.setString(5, user.getType().toString());
+			statement.setBoolean(6, user.isBanned());
+			statement.setString(7, user.getConfirmCode());
+			statement.setString(8, user.getStatus().toString());
+			statement.setString(9, user.getPhoneNumber());
+			statement.setString(10, user.getImage());
+			statement.setString(11, user.getSocialNetwork().toString());
+			statement.setString(12, user.getSocialNetworkId());
+
+			statement.setInt(13, user.getId());
+			return statement.executeUpdate();
+		}
+	}
 }
--- src/main/java/com/epam/task/database/model/User.java
+++ src/main/java/com/epam/task/database/model/User.java
@@ -1,5 +1,139 @@
 package com.epam.task.database.model;
 
+import com.epam.task.database.model.enums.SocialNetwork;
+import com.epam.task.database.model.enums.UserStatus;
+import com.epam.task.database.model.enums.UserType;
+import com.epam.task.database.transformers.DataBaseField;
+
 public class User {
+	@DataBaseField(fieldName = "user_id")
+	private int id;
+	@DataBaseField(fieldName = "first_name")
+	private String firstName;
+	@DataBaseField(fieldName = "last_name")
+	private String lastName;
+	@DataBaseField(fieldName = "e_mail")
+	private String email;
+	@DataBaseField(fieldName = "password")
+	private String password;
+	@DataBaseField(fieldName = "type")
+	private UserType type; //enum
+	@DataBaseField(fieldName = "is_banned")
+	private boolean isBanned;
+	@DataBaseField(fieldName = "confirm_code")
+	private String confirmCode;
+	@DataBaseField(fieldName = "status")
+	private UserStatus status; //enum
+	@DataBaseField(fieldName = "phone_number")
+	private String phoneNumber;
+	@DataBaseField(fieldName = "img")
+	private String image;
+	@DataBaseField(fieldName = "social_network")
+	private SocialNetwork socialNetwork; //enum
+	@DataBaseField(fieldName = "social_network_id")
+	private String socialNetworkId;
+	public User(int id, String firstName, String lastName, String email, String password, String type,
+			boolean isBanned, String confirmCode, String status, String phoneNumber, String image,
+			String socialNetwork, String socialNetworkId) {
+		
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.type = UserType.valueOf(type);
+		this.isBanned = isBanned;
+		this.confirmCode = confirmCode;
+		this.status = UserStatus.valueOf(status);
+		this.phoneNumber = phoneNumber;
+		this.image = image;
+		this.socialNetwork = SocialNetwork.valueOf(socialNetwork);
+		this.socialNetworkId = socialNetworkId;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public UserType getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = UserType.valueOf(type);
+	}
+	public boolean isBanned() {
+		return isBanned;
+	}
+	public void setBanned(boolean isBanned) {
+		this.isBanned = isBanned;
+	}
+	public String getConfirmCode() {
+		return confirmCode;
+	}
+	public void setConfirmCode(String confirmCode) {
+		this.confirmCode = confirmCode;
+	}
+	public UserStatus getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = UserStatus.valueOf(status);
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public SocialNetwork getSocialNetwork() {
+		return socialNetwork;
+	}
+	public void setSocialNetwork(String socialNetwork) {
+		this.socialNetwork = SocialNetwork.valueOf(socialNetwork);
+	}
+	public String getSocialNetworkId() {
+		return socialNetworkId;
+	}
+	public void setSocialNetworkId(String socialNetworkId) {
+		this.socialNetworkId = socialNetworkId;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", type=" + type + ", isBanned=" + isBanned + ", confirmCode="
+				+ confirmCode + ", status=" + status + ", phoneNumber=" + phoneNumber + ", image=" + image
+				+ ", socialNetwork=" + socialNetwork + ", socialNetworkId=" + socialNetworkId + "]";
+	}
 	
 }
