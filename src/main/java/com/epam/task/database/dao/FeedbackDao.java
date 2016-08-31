a99bde6901a1ed7dd958e079d6e885329a0c8a45
user1\Администратор <hjvfyfyfy@gmail.com>
2016-08-31 15:26:46 +0300

    update

---
+++ src/main/java/com/epam/task/database/dao/FeedbackDao.java
@@ -0,0 +1,77 @@
+package com.epam.task.database.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Collection;
+
+import com.epam.task.database.model.Feedback;
+import com.epam.task.database.transformers.UniversalTransformer;
+
+public class FeedbackDao {
+	private Connection connection;
+	private final String SELECT_ALL_FEEDBACK = "SELECT * FROM feedback";
+	private final String INSERT_FEEDBACK = "INSERT INTO feedback VALUES (?, ?, ?, ?, ?, ?)";
+	private final String UPDATE_FEEDBACK = "UPDATE INTO feedback SET user_id = ?, hotel_id = ?, rating = ?, comment = ?, title = ?, date = ? WHERE feedback_id = ?";
+	private final String DELETE_FEEDBACK = "DELETE FROM feedback WHERE feedback_id = ?";
+
+	public FeedbackDao(Connection connection) {
+		super();
+		this.connection = connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	public Collection<Feedback> getAllFeedbacks() {
+		try (PreparedStatement statment = connection.prepareStatement(SELECT_ALL_FEEDBACK)) {
+			ResultSet rs = statment.executeQuery();
+			return UniversalTransformer.getCollectionFromRS(rs, Feedback.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public int insertFeedback(Feedback element) {
+		try (PreparedStatement statment = connection.prepareStatement(INSERT_FEEDBACK)) {
+			statment.setInt(1, element.getUserId());
+			statment.setInt(2, element.getUserId());
+			statment.setInt(3, element.getUserId());
+			statment.setString(4, element.getComment());
+			statment.setString(5, element.getTitle());
+			statment.setTimestamp(6, element.getDate());
+			return statment.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+	public int updateFeedback(Feedback element) {
+		try (PreparedStatement statment = connection.prepareStatement(UPDATE_FEEDBACK)) {
+			statment.setInt(1, element.getUserId());
+			statment.setInt(2, element.getUserId());
+			statment.setInt(3, element.getUserId());
+			statment.setString(4, element.getComment());
+			statment.setString(5, element.getTitle());
+			statment.setTimestamp(6, element.getDate());
+			statment.setInt(7, element.getId());
+			return statment.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+	public int deleteFeedback(Feedback element) {
+		try (PreparedStatement statment = connection.prepareStatement(DELETE_FEEDBACK)) {
+			statment.setInt(1, element.getId());
+			return statment.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+}
