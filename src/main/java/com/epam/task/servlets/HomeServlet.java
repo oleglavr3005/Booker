8044e5ecbb2f527cff67c88ff190ebbdc9a98cd4
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-01 00:51:35 +0300

    Home Servlet (just redirects on home page)

---
+++ src/main/java/com/epam/task/servlets/HomeServlet.java
@@ -0,0 +1,26 @@
+package com.epam.task.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public HomeServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("pages/index.jsp");
+		//or
+		//request.getRequestDispatcher("pages/index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
