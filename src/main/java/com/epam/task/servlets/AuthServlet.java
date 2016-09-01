919ac4a2ca7655c9e44b1ff0c92d50203c5c1dc8
Stodilko <Batman@BatmanComputer>
2016-09-01 17:50:03 +0300

    Added Auth Servlet

---
+++ src/main/java/com/epam/task/servlets/AuthServlet.java
@@ -0,0 +1,61 @@
+package com.epam.task.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epam.task.database.model.User;
+import com.epam.task.database.model.enums.UserStatus;
+import com.epam.task.database.model.enums.UserType;
+import com.epam.task.database.service.UserService;
+import com.epam.task.util.PasswordHasher;
+
+/**
+ * Servlet implementation class AuthServlet
+ */
+@WebServlet("/auth")
+public class AuthServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AuthServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			UserService userService = new UserService();
+			User user = userService.getUserByEmail(request.getParameter("email"));
+			if (user.getStatus().equals(UserStatus.PENDING) || !user.getPassword()
+					.equals(PasswordHasher.hash(request.getParameter("password") + (user.getFirstName())))) {
+				throw new Exception();
+			}
+			request.getSession().setAttribute("userID", user.getId());
+			request.getSession().setAttribute("userState", user.getType());
+			response.getWriter().write(user.getType() != UserType.ADMIN ? "cabinet" : "admin");
+		} catch (Exception e) {
+			response.setContentType("text/plain");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write("false");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
