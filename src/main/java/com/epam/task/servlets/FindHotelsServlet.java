4483c474f1baba22815b89715c1f064f41bca7cc
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-01 12:48:53 +0300

    Created a servlet for hotel search.

---
+++ src/main/java/com/epam/task/servlets/FindHotelsServlet.java
@@ -0,0 +1,85 @@
+package com.epam.task.servlets;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epam.task.database.model.Hotel;
+import com.epam.task.database.service.HotelService;
+
+@WebServlet("/search")
+public class FindHotelsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public FindHotelsServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = "Hotel1"; //get from request
+		int minStars = 3; //get from request
+		int maxStars = 5; //get from request
+		int people = 5; //get from request
+		
+		boolean typeStandart = true; //get from request
+		boolean typeLux = true; //get from request
+		boolean typeDelux = true; //get from request
+
+		boolean foodNone = true; //get from request
+		boolean foodBreakfast = true; //get from request
+		boolean foodTwice = true; //get from request
+		boolean foodFull = true; //get from request
+		
+		int minPrice = 1; //get from request
+		int maxPrice = 500; //get from request
+
+		boolean hasWiFi = true; //get from request
+		boolean hasShower = true; //get from request
+		boolean hasParking = true; //get from request
+		boolean hasCondition = true; //get from request
+		boolean hasPool = true; //get from request
+		boolean hasGym = true; //get from request
+		boolean hasBalcony = true; //get from request
+
+		boolean noDeposit = true; //get from request
+		
+		Timestamp startDate;
+		Timestamp endDate;
+		try {
+			startDate = new Timestamp(new SimpleDateFormat("yyyy-MM-dd").parse("2016-09-22").getTime());
+		} catch (ParseException e) {
+			startDate = new Timestamp(new Date().getTime());
+		}
+		try {
+			endDate = new Timestamp(new SimpleDateFormat("yyyy-MM-dd").parse("2016-09-26").getTime());
+		} catch (ParseException e) {
+			endDate = new Timestamp(new Date().getTime());
+		}
+		//
+		List<Hotel> suitableHotels = new HotelService().getAllSuitableHotels(name, minStars, maxStars, people, 
+				typeStandart, typeLux, typeDelux, 
+				foodNone, foodBreakfast, foodTwice, foodFull, 
+				minPrice, maxPrice, 
+				hasWiFi, hasShower, hasParking, hasCondition, hasPool, hasGym, hasBalcony, noDeposit, 
+				startDate, endDate);
+		
+		request.setAttribute("hotels", suitableHotels);
+		request.setAttribute("countOfHotels", suitableHotels.size());
+		
+		request.getRequestDispatcher("pages/index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
