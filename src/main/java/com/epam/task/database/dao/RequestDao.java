4b148d68c9bcd22069dbe9cb5f23aac08e4c5d47
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-04 20:32:35 +0300

    Request DAO ready

--- src/main/java/com/epam/task/database/dao/RequestDao.java
+++ src/main/java/com/epam/task/database/dao/RequestDao.java
@@ -1,15 +1,24 @@
 package com.epam.task.database.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.epam.task.database.model.Request;
+import com.epam.task.database.model.enums.RequestStatus;
+import com.epam.task.database.transformers.UniversalTransformer;
 
 public class RequestDao {
 
 	private Connection connection;
 	
 	private final String SELECT_ALL = "SELECT * FROM `request`";
+	private final String SELECT_ALL_BY_STATUS = "SELECT * FROM `request` WHERE status LIKE ?";
 	private final String SELECT_BY_ID = "SELECT * FROM `request` WHERE request_id = ?";
 	private final String SELECT_BY_USER = "SELECT * FROM `request` WHERE user_id = ?";
-	private final String SELECT_BY_STATUS = "SELECT * FROM `request` WHERE status LIKE ?";
 	
 	private final String INSERT_REQUEST = "INSERT INTO `request` (user_id, message, request_date, status) VALUES (?, ?, ?, ?)";
 	private final String UPDATE_REQUEST = "UPDATE `request` SET user_id = ?, message = ?, request_date = ?, status = ? WHERE request_id = ?";
@@ -24,4 +33,91 @@ public class RequestDao {
 		this.connection = connection;
 	}
 
+	public List<Request> getAllRequests() {
+		try (PreparedStatement statement = connection.prepareStatement(SELECT_ALL);
+					ResultSet result = statement.executeQuery()) {
+			return UniversalTransformer.getCollectionFromRS(result, Request.class);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return new ArrayList<>();
+		}
+	}
+
+	public List<Request> getAllRequestsByStatus(RequestStatus status) {
+		try (PreparedStatement statement = connection.prepareStatement(SELECT_ALL_BY_STATUS)) {
+			statement.setString(1, status.toString());
+			try (ResultSet result = statement.executeQuery()) {
+				return UniversalTransformer.getCollectionFromRS(result, Request.class);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return new ArrayList<>();
+		}
+	}
+	
+	public Request getRequestById(int id) {
+		try (PreparedStatement statement = connection.prepareStatement(SELECT_BY_ID)) {
+			statement.setInt(1, id);
+			try (ResultSet result = statement.executeQuery()) {
+				return UniversalTransformer.getObjectFromRS(result, Request.class);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public Request getRequestByUserId(int userId) {
+		try (PreparedStatement statement = connection.prepareStatement(SELECT_BY_USER)) {
+			statement.setInt(1, userId);
+			try (ResultSet result = statement.executeQuery()) {
+				return UniversalTransformer.getObjectFromRS(result, Request.class);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public int insertRequest(Request request) {
+		try (PreparedStatement statement = connection.prepareStatement(INSERT_REQUEST)) {
+			int i = 1;
+			statement.setInt(i++, request.getUserId());
+			statement.setString(i++, request.getMessage());
+			statement.setTimestamp(i++, request.getRequestDate());
+			statement.setString(i++, request.getStatus().toString());
+			return statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public int updateRequest(Request request) {
+		try (PreparedStatement statement = connection.prepareStatement(UPDATE_REQUEST)) {
+			int i = 1;
+			statement.setInt(i++, request.getUserId());
+			statement.setString(i++, request.getMessage());
+			statement.setTimestamp(i++, request.getRequestDate());
+			statement.setString(i++, request.getStatus().toString());
+			
+			statement.setInt(i++, request.getId());
+			return statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public int updateRequestStatus(int requestId, RequestStatus status) {
+		try (PreparedStatement statement = connection.prepareStatement(CHANGE_REQUEST_STATUS)) {
+			int i = 1;
+			statement.setInt(i++, requestId);
+			statement.setString(i++, status.toString());
+			return statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
 }
--- src/main/java/com/epam/task/database/service/RequestService.java
+++ src/main/java/com/epam/task/database/service/RequestService.java
@@ -1,5 +1,45 @@
 package com.epam.task.database.service;
 
-public class RequestService {
+import java.util.List;
 
+import com.epam.task.database.dao.manager.DaoManager;
+import com.epam.task.database.model.Request;
+import com.epam.task.database.model.enums.RequestStatus;
+
+public class RequestService {
+	
+	private DaoManager daoManager;
+	
+	public RequestService() {
+		super();
+		daoManager = new DaoManager();
+	}
+
+	public List<Request> getAllRequests() {
+		return daoManager.executeAndClose(() -> daoManager.getRequestDao().getAllRequests());
+	}
+
+	public List<Request> getAllRequestsByStatus(RequestStatus status) {
+		return daoManager.executeAndClose(() -> daoManager.getRequestDao().getAllRequestsByStatus(status));
+	}
+
+	public Request getRequestById(int id) {
+		return daoManager.executeAndClose(() -> daoManager.getRequestDao().getRequestById(id));
+	}
+
+	public Request getRequestByUserId(int id) {
+		return daoManager.executeAndClose(() -> daoManager.getRequestDao().getRequestByUserId(id));
+	}
+
+	public int insertRequest(Request request) {
+		return daoManager.executeAndClose(() -> daoManager.getRequestDao().insertRequest(request));
+	}
+
+	public int updateRequest(Request request) {
+		return daoManager.executeAndClose(() -> daoManager.getRequestDao().updateRequest(request));
+	}
+
+	public int updateRequestStatus(int requestId, RequestStatus status) {
+		return daoManager.executeAndClose(() -> daoManager.getRequestDao().updateRequestStatus(requestId, status));
+	}
 }
