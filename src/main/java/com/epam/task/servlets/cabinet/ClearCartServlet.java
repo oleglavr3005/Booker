880d2a65f1242f8b5d27ce7e4df8abeee4db1938
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-04 11:34:05 +0300

    Clear cart servlet mods

--- src/main/java/com/epam/task/servlets/cabinet/ClearCartServlet.java
+++ src/main/java/com/epam/task/servlets/cabinet/ClearCartServlet.java
@@ -1,12 +1,15 @@
 package com.epam.task.servlets.cabinet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.epam.task.database.model.Order;
 import com.epam.task.database.model.User;
 import com.epam.task.database.model.enums.OrderStatus;
 import com.epam.task.database.service.OrderService;
@@ -21,9 +24,11 @@ public class ClearCartServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int userId = ((User) request.getSession().getAttribute("user")).getId();
-		int removed = new OrderService().removeAllOrdersByStatus(userId, OrderStatus.ORDER);
+		new OrderService().removeAllOrdersByStatus(userId, OrderStatus.ORDER);
 		
-		response.getWriter().write(removed > 0 ? "true" : "false");
+//		response.getWriter().write(removed > 0 ? "true" : "false");
+		request.setAttribute("orders", new ArrayList<Order>());
+		request.getRequestDispatcher("/pages/user/shopping_cart.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
