1cb144f7795b58e07758436f832c675d3773dd67
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-04 23:18:20 +0300

    Settings servlets

---
+++ src/main/java/com/epam/task/servlets/settings/ChangeInfoServlet.java
@@ -0,0 +1,45 @@
+package com.epam.task.servlets.settings;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.epam.task.database.model.User;
+import com.epam.task.database.service.UserService;
+
+//change last name and first name
+@WebServlet("/change_info")
+public class ChangeInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ChangeInfoServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String lastName = request.getParameter("lastName");
+		String firstName = request.getParameter("firstName");
+		
+		if(lastName == null || firstName == null) {
+			return;
+		}
+		
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+		user.setLastName(lastName);
+		user.setFirstName(firstName);
+		int result = new UserService().updateUser(user);
+		session.setAttribute("user", user);
+		response.getWriter().write(result > 0 ? "true" : "false");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
