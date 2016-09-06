5509b541fcb9fb4eaf2c08cc375e92f45816df1e
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-06 16:40:40 +0300

    Manager Room servlet

---
+++ src/main/java/com/epam/task/servlets/manager/ManagerRoomServlet.java
@@ -0,0 +1,42 @@
+package com.epam.task.servlets.manager;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epam.task.database.model.Room;
+import com.epam.task.database.model.User;
+import com.epam.task.database.service.RoomService;
+
+@WebServlet("/cabinet/room/*" )
+public class ManagerRoomServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ManagerRoomServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User manager = (User) request.getSession().getAttribute("user");
+
+		int id;
+		try {
+			id = Integer.parseInt(request.getPathInfo().substring(1));
+		} catch (Exception e) {
+			return;
+		}
+		
+		Room room = new RoomService().getRoomById(id);
+		
+		request.setAttribute("room", room);
+		request.getRequestDispatcher("/pages/manager/roomConcret.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
