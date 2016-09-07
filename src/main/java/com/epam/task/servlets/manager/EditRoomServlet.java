ca19d9e8b6604965f164d23af47e1ce349bddc47
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-07 15:46:19 +0300

    Room updating and deleting stuff

---
+++ src/main/java/com/epam/task/servlets/manager/EditRoomServlet.java
@@ -0,0 +1,100 @@
+package com.epam.task.servlets.manager;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epam.task.database.model.Room;
+import com.epam.task.database.service.RoomService;
+
+@WebServlet("/edit_room")
+public class EditRoomServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public EditRoomServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String roomIdString = request.getParameter("roomId");
+		String number = request.getParameter("number");
+		String type = request.getParameter("type");
+		
+		String doubleBedsCountString = request.getParameter("doubleBedsCount");
+		String bedsCountString = request.getParameter("bedsCount");
+		
+		String priceString = request.getParameter("price");
+		
+		String hasWiFiString = request.getParameter("hasWiFi");
+		String hasShowerString = request.getParameter("hasShower");
+		String hasParkingString = request.getParameter("hasParking");
+		String hasConditionString = request.getParameter("hasCondition");
+		String hasPoolString = request.getParameter("hasPool");
+		String hasGymString = request.getParameter("hasGym");
+		String hasBalconyString = request.getParameter("hasBalcony");
+		
+		String food = request.getParameter("food");
+
+		String freeBook = request.getParameter("freeBook");
+		
+		if (roomIdString == null || number == null || type == null || doubleBedsCountString == null || bedsCountString == null ||
+				priceString == null || food == null) {
+			return;
+		}
+
+		int roomId = Integer.parseInt(roomIdString);
+		int doubleBedsCount = Integer.parseInt(doubleBedsCountString);
+		int bedsCount = Integer.parseInt(bedsCountString);
+		int price = Integer.parseInt(priceString);
+		
+		boolean hasWifi = hasWiFiString == null ? false : true;
+		boolean hasShower = hasShowerString == null ? false : true;
+		boolean hasParking = hasParkingString == null ? false : true;
+		boolean hasCondition = hasConditionString == null ? false : true;
+		boolean hasPool = hasPoolString == null ? false : true;
+		boolean hasGym = hasGymString == null ? false : true;
+		boolean hasBalcony = hasBalconyString == null ? false : true;
+		int daysCount;
+		int percentage;
+		
+		if(freeBook != null) {
+			daysCount = Integer.parseInt(request.getParameter("daysCount"));
+			percentage = Integer.parseInt(request.getParameter("percentage"));
+		} else {
+			daysCount = -1;
+			percentage = 0;
+		}
+		
+		RoomService roomService = new RoomService();
+		Room room = roomService.getRoomById(roomId);
+		room.setNumber(number);
+		room.setType(type);
+		room.setBedsCount(bedsCount);
+		room.setDoubleBedsCount(doubleBedsCount);
+		room.setPrice(price);
+		room.setFood(food);
+		
+		room.setWifi(hasWifi);
+		room.setShower(hasShower);
+		room.setParking(hasParking);
+		room.setCondition(hasCondition);
+		room.setPool(hasPool);
+		room.setGym(hasGym);
+		room.setBalcony(hasBalcony);
+
+		room.setDaysCount(daysCount);
+		room.setPercentage(percentage);
+		
+		int changed = roomService.updateRoom(room);
+	
+		response.getWriter().write(changed > 0 ? "true" : "false");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
