a15a181908fc2643973d56bb8f66d35f90318ede
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-10 10:04:26 +0300

    Create room filter

---
+++ src/main/java/com/epam/task/filters/CreateRoomFilter.java
@@ -0,0 +1,49 @@
+package com.epam.task.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.epam.task.database.model.User;
+import com.epam.task.database.model.enums.UserType;
+import com.epam.task.database.service.HotelService;
+
+@WebFilter("/cabinet/create_room")
+public class CreateRoomFilter implements Filter {
+
+    public CreateRoomFilter() {
+    }
+
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession httpSession = httpRequest.getSession(true);
+		
+		User user = (User) httpSession.getAttribute("user");
+		
+		if(user != null && user.getType() == UserType.MANAGER) {
+			if(!new HotelService().getAllHotelsByManager(user.getId()).isEmpty()) {
+				chain.doFilter(request, response);
+			} else {
+				((HttpServletResponse) response).sendError(404);
+			}
+		} else {		//throw the unexpected visitor on the error page
+			((HttpServletResponse) response).sendError(404);
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
