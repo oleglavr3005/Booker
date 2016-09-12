87ff0046ea4d784a62762a96fb8015de93db4191
Stodilko <Batman@BatmanComputer>
2016-09-12 11:10:02 +0300

    modal seems good

--- src/main/webapp/resources/js/test/recommendModal.js
+++ src/main/webapp/resources/js/test/recommendModal.js
@@ -2,15 +2,20 @@ function testFunction(nmb) {
 	$.post('../get_hotels', {
 		hotelsNum : nmb
 	}, function(result) {
-		alert("comeBack");
+		debugger;
 		var res = $.parseJSON(result);
 		showModal(res);
 		$('#modal1').openModal({
 			complete : function() {
-				alert("onHide");
 				document.getElementById('empty').style.display = 'none';
 				for (var i = 0; i < 3; i++) {
 					document.getElementById('row' + i).style.display = 'none';
+					for(var y = 0; y < 5;y++){
+						if ($('#'+ i + y).hasClass('fa-star')){
+							$('#'+ i + y).removeClass("fa-star");
+							$('#'+ i + y).addClass("fa-star-o");
+						}
+					}
 				}
 				$.get('../refresh_cart', {
 					compareBy : $('#compare').val(),
@@ -29,6 +34,11 @@ function showModal(result) {
 	if (result.countOfHotels > 0) {
 		var url = 'http://localhost:8080/booker/hotel/';
 		for (var i = 0; i < 3 && i < result.countOfHotels; i++) {
+		//	alert(result.hotels[i].stars);
+			for(var y = 0; y < result.hotels[i].stars;y++){
+				$('#'+ i + y).removeClass("fa-star-o");
+				$('#'+ i + y).addClass("fa-star");
+			}
 			document.getElementById('row' + i).style.display = 'block';
 			$('#href' + i + '1').attr('href', url + result.hotels[i].id);
 			$('#href' + i + '2').attr('href', url + result.hotels[i].id);
@@ -37,11 +47,7 @@ function showModal(result) {
 			$('#hotelName' + i).html(result.hotels[i].name);
 			$('#location' + i).html(
 					result.hotels[i].city + " " + result.hotels[i].street);
-			alert(result.hotels[i].stars);
-			for(var y = 0; y < result.hotels[i].stars;y++){
-				$('#'+ i + y).removeClass("fa-star-o");
-				$('#'+ i + y).addClass("fa-star");
-			}
+			
 		}
 	} else {
 		document.getElementById('empty').style.display = 'block';
