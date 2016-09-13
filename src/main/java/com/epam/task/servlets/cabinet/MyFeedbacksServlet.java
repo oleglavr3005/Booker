b67dbdd363ef6f2457c62237a22eddfbef0bafdd
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-13 17:11:36 +0300

    Backend pagination for feedbacks

--- src/main/java/com/epam/task/servlets/cabinet/MyFeedbacksServlet.java
+++ src/main/java/com/epam/task/servlets/cabinet/MyFeedbacksServlet.java
@@ -24,9 +24,30 @@ public class MyFeedbacksServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		User user = (User) request.getSession().getAttribute("user");
-		List<Feedback> lisrFeedback = new FeedbackService().getAllFeedbacksByUser(user.getId());
-		request.setAttribute("feedbacks", FeedbackDto.listConverter(lisrFeedback));
-		request.getRequestDispatcher("/pages/user/myFeedbacks.jsp").forward(request, response);
+		
+		String pageString = request.getParameter("page");
+		int page = pageString == null ? 1 : Integer.parseInt(pageString);
+		List<Feedback> listFeedback = new FeedbackService().getAllFeedbacksByUser(user.getId());
+		int countOfFeedbacks = listFeedback.size();
+		
+		int countOfPages = (int) Math.ceil(countOfFeedbacks / 5.0);
+		if (page > countOfPages) {
+			page--;
+		}
+		
+		String compareBy = request.getParameter("compareBy");  //compareByDateAsc compareByDateDesc
+		List<Feedback> listFeedbackByPage = new FeedbackService().getAllFeedbacksByUserAndPage(user.getId(), page, compareBy);
+
+		request.setAttribute("countOfFeedbacks", countOfFeedbacks);
+		request.setAttribute("countOfPages", countOfPages);
+		request.setAttribute("feedbacks", FeedbackDto.listConverter(listFeedbackByPage));
+		request.setAttribute("currentPage", page);
+		
+		if(request.getParameter("flag") != null && request.getParameter("flag").equals("true")) {
+			request.getRequestDispatcher("/pages/comments/commentCard.jsp").forward(request, response);
+		} else {
+			request.getRequestDispatcher("/pages/user/myFeedbacks.jsp").forward(request, response);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
