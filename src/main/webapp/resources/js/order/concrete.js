e7131b0e9ceb20a34cc114034f804ea1a77e07d9
Stodilko <Batman@BatmanComputer>
2016-09-14 23:58:26 +0300

    started wok with script for commentpdate

--- src/main/webapp/resources/js/order/concrete.js
+++ src/main/webapp/resources/js/order/concrete.js
@@ -1,3 +1,35 @@
 function updateComment(){
+	var comment = $('#comment').html();
+	if (validateComment(comment)){
+		alert("post to update");
+	}
+	else{
+		alert("invalid");
+	}
 	alert("WRTIE THIS FUCNTION IN ORDER/CONCRETE.JS");
+}
+
+function textIsValid(field) {
+	var re = /^([-a-zA-Zа-яА-Я0-9іІьїЇєЄ’.!/'" ]*)$/;
+	return re.test(field);
+}
+
+function validateComment(comment) {
+	if (comment.length >= 10 && comment.length <= 1000 && textIsValid(comment)) {
+		valid('comment');
+		return true;
+	} else {
+		invalid('comment');
+		return false;
+	}
+}
+
+function valid(field) {
+	$('#' + field).removeClass("invalid");
+	$('#' + field).addClass("valid");
+}
+
+function invalid(field) {
+	$('#' + field).removeClass("valid");
+	$('#' + field).addClass("invalid");
 }
