ef76bf449df3b34a1fc0d7f17bf2fc59697b19ea
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-15 17:07:51 +0300

    Trying to get hotelId parameter

--- src/main/java/com/epam/task/servlets/manager/RoomCreatePageServlet.java
+++ src/main/java/com/epam/task/servlets/manager/RoomCreatePageServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.epam.task.database.model.User;
 import com.epam.task.database.service.HotelService;
+import com.epam.task.util.StringUtil;
 
 @WebServlet("/cabinet/create_room")
 public class RoomCreatePageServlet extends HttpServlet {
@@ -19,7 +20,14 @@ public class RoomCreatePageServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String hotelIdString = request.getParameter("hotelId");
+		if(!StringUtil.isPositiveInteger(hotelIdString)) {
+			response.sendError(500);
+			return;
+		}
+		
 		int managerId = ((User) request.getSession().getAttribute("user")).getId();
+		request.setAttribute("hotelId", Integer.parseInt(hotelIdString));
 		request.setAttribute("hotels", new HotelService().getAllHotelsByManager(managerId));
 		request.getRequestDispatcher("/pages/manager/roomCreate.jsp").forward(request, response);
 	}
