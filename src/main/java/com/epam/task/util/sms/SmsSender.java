6a323c1d3c2b57844e203341716c57560218e4c2
Stodilko <Batman@192.168.210.50>
2016-09-15 14:12:40 +0300

    added sms api

---
+++ src/main/java/com/epam/task/util/sms/SmsSender.java
@@ -0,0 +1,27 @@
+package com.epam.task.util.sms;
+
+public class SmsSender {
+	
+	private static final String LOGIN = "lighthouse_group"; 
+	private static final String PASS = "12345678";
+	
+
+
+//	private static Logger LOG = Logger.getLogger(SmsSender.class);
+//	static {
+//		DOMConfigurator.configure("src/main/java/com/xml/Warn.xml");
+//	}
+	
+
+	public static void sendSms(String number, String text, int nmb) {
+
+		SendApi sd = new SendApi(LOGIN, PASS, "utf-8", true);
+		System.out.println(text);
+	//	sd.sendSms(number, text, 0, "", "", 0, "", "");
+		sd.getBalance();
+
+	}
+	
+	
+
+}
