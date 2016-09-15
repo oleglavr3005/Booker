bc9749cdfea5138e19ddcba34549ccd0aeeb4a6a
Melnyk <hjvfyfyfy@gmail.com>
2016-09-15 13:43:15 +0300

    fix inter

--- src/main/webapp/resources/js/internalization.js
+++ src/main/webapp/resources/js/internalization.js
@@ -677,8 +677,6 @@
 		    message_error : 'Can not add comment',
 		    info_toggle_open : 'Show additional info',
 		    info_toggle_hide : 'Hide additional info',
-		    first : "First",
-		    last : "Last",
 		    hotel : {
 		    	createSucces : "hotel was created",
 		    	createFail : "hotel wasnt created",
@@ -726,8 +724,6 @@
 		    message_error : 'Неможли додати коментар',
 		    info_toggle_open : 'Показати додаткову інформацію',
 		    info_toggle_hide : 'Сховати додаткову інформацію',
-		    first : "Попередня",
-		    last : "Наступна",
 		    hotel : {
 		    	createSucces : "Готель створений",
 		    	createFail : "Готель не був створений",
