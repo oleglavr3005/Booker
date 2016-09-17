811a655bc3262c4bfb9e85926fb0a27882cf0e25
NymphadoraTonks <lelich.katerina@mail.ru>
2016-09-17 15:01:06 +0300

    New filter

---
+++ src/main/java/com/epam/task/filters/ActiveUserFilter.java
@@ -0,0 +1,44 @@
+package com.epam.task.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.epam.task.database.model.User;
+import com.epam.task.database.model.enums.UserStatus;
+
+@WebFilter({ "/add_to_cart", "/remove_from_cart", "/clear_cart", "/book_all", "/book", 
+	"/check_order", "/create_request", "/add_feedback", "/change_feedback", "/deleteFeedback" })
+public class ActiveUserFilter implements Filter {
+
+    public ActiveUserFilter() {
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession httpSession = httpRequest.getSession(true);
+		
+		User user = (User) httpSession.getAttribute("user");
+		
+		if(user != null && user.getStatus() == UserStatus.ACTIVE) {
+			chain.doFilter(request, response);
+		} else {		//throw the unexpected visitor on the error page
+			((HttpServletResponse) response).sendError(404);
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
